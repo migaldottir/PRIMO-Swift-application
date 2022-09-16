f369e6cc2f51e6159d26da1c4255c80d5cbcd8c2
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-17 01:00:03 +0200

    added button and set color for it

--- Primo/ViewController.swift
+++ Primo/ViewController.swift
@@ -14,16 +14,38 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .cyan
         return imageView
     }()
-
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemPink
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(button)
+        button.frame = CGRect(x: 20, y: view.frame.size.height-50-view.safeAreaInsets.bottom, width: view.frame.size.width-40, height: 50)
+        
+        getRandomPhoto()
     }
-
-
+    func getRandomPhoto() {
+        let urlString =
+        "http://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage (data: data)
+    }
+    
+   
 }
+
+
+
 
