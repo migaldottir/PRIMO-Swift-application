e6edc488ce5470921011305f1473b399c8c1d746
Astryda Malinowska <astamalinowska@gmail.com>
2022-09-16 23:15:28 +0200

    Initial Commit

---
+++ Primo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Primo
+//
+//  Created by Refr Asta on 16/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
